a2fb8710158fa7900a9a0c6394df2864ef0076f6
raghav-1998 <raghav.vasdeva@gmail.com>
2024-03-27 14:54:44 +0530

    Adding refreshToken field & function for generating access & refresh token

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 //Created User Schema
 const userSchema=new mongoose.Schema({
@@ -25,6 +26,9 @@ const userSchema=new mongoose.Schema({
     password:{
         type:String,
         required:true
+    },
+    refreshToken:{
+        type:String,
     }
 },{timestamps:true})
 
@@ -38,6 +42,27 @@ userSchema.pre("save",async function(next){
 
 userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        userName:this.userName,
+        fullName:this.fName+' '+this.lName
+    },process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id:this._id,
+    },process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
 }
 
 //Exporting User model of schema userSchema
