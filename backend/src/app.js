0c23509d93777574266e22fbf05ef4797b1769ad
raghav-1998 <raghav.vasdeva@gmail.com>
2024-03-27 14:56:41 +0530

    Complete configuration & add root route for user

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 const app=express()
 
@@ -11,14 +12,8 @@ app.use(cors(corsOption))// Passing the options to configure CORS
 app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Welcome To TaskBook</h1>")
-})
-
-
-app.get('/login',(req,res)=>{
-    res.send("<h2>Please Login</h2>")
-})
+import userRouter from './routes/user.routes.js'
+app.use('/user',userRouter)
 
 export default app;
 
