e2097ceeb2d0613bec0008effc3a27908d2497f7
raghav-1998 <raghav.vasdeva@gmail.com>
2024-03-29 13:58:22 +0530

    Adding Change Password Route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser } from "../controllers/user.controllers.js";
+import { registerUser,loginUser, logoutUser, changeCurrentPassword } from "../controllers/user.controllers.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router=Router()
@@ -7,5 +7,6 @@ const router=Router()
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJwt,logoutUser)
+router.route('/change-current-password').post(changeCurrentPassword)
 
 export default router;
