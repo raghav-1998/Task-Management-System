f45eedf10d838aaa4cbddcb0a6deb74b66e7629f
raghav-1998 <raghav.vasdeva@gmail.com>
2024-03-30 13:18:02 +0530

    Adding more task related controllers

--- backend/src/controllers/task.controllers.js
+++ backend/src/controllers/task.controllers.js
@@ -32,10 +32,112 @@ const createTask=async(req,res,next)=>{
     }
 }
 
+const getAllTasks=async(req,res,next)=>{
+    try {
+        try {
+            const userId=req.user._id
+        
+            const tasks=await Task.find({addedBy:userId})
 
-const deleteAllTasksOfUser=async(req,res,next)=>{
-    
+            if(tasks.length===0){
+                return res
+                .status(404)
+                .json({
+                    statusCode:404,
+                    message:"No task found for this User"
+                })
+            }
+
+            return res
+            .status(200)
+            .json({
+                statusCode:200,
+                data:tasks,
+                message:"Tasks fetched successfully"
+            })
+            
+        } catch (error) {
+            throw new Error("Something went wrong while fetch User's Tasks")
+        }
+    } catch (error) {
+        next(error)
+    }   
 }
+
+/*
+const getTaskById=async(req,res,next)=>{
+    try {
+        req.param.
+        
+    } catch (error) {
+        next(error)
+    }
+}
+*/
+
+const deleteAllTasks=async(req,res,next)=>{
+    try {
+        try {
+            const userId=req.user._id
+
+            const tasks=await Task.find({addedBy:userId})
+
+            if(tasks.length===0){
+                return res
+                .status(404)
+                .json({
+                    statusCode:404,
+                    message:"No task found for this User"
+                })
+            }
+
+            await Task.deleteMany({addedBy:userId})
+
+            return res
+            .status(200)
+            .json({
+                statusCode:200,
+                message:"Task deleted sucessfully"
+            })
+        } catch (error) {
+            throw new Error("Error while deleting tasks of user")
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+const deleteTaskById=async(req,res,next)=>{
+    try {
+        try {
+            const {taskId}=req.params
+        
+            const taskToBeDeleted=await Task.findById(taskId)
+        
+            if(!taskToBeDeleted){
+                throw new Error("Task Not found")
+            }
+        
+            await Task.findByIdAndDelete(taskId)
+        
+            return res
+            .status(200)
+            .json({
+                statusCode:200,
+                message:"Task Deleted Successfully"
+            })
+
+        } catch (error) {
+            throw new Error("Error while deleting task of User")
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
 export{
-    createTask
+    createTask,
+    getAllTasks,
+    deleteAllTasks,
+    deleteTaskById
 }
