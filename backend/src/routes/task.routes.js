04fa1df3ef1669d280d3ffbce64d5e59e4b71f1d
raghav-1998 <raghav.vasdeva@gmail.com>
2024-03-30 13:19:59 +0530

    Adding more task related routes

--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express"
-import { createTask } from "../controllers/task.controllers.js"
+import { createTask, deleteAllTasks, deleteTaskById, getAllTasks } from "../controllers/task.controllers.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js"
 
 const router=Router()
 
-router.route('/create-task').post(verifyJwt,createTask)
+router.route('/').post(verifyJwt,createTask)
+router.route('/').get(verifyJwt,getAllTasks)
+router.route('/').delete(verifyJwt,deleteAllTasks)
+
+router.route('/:taskId').delete(deleteTaskById)
+
 
 export default router
